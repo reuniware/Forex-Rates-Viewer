2f83e33d05be5bb625ac73e9685587c4bdff38b2
Quant & Fintech Opensource Projects <reunisoft@gmail.com>
2018-10-04 15:04:30 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForexRatesViewer
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
